768de2fe6e763bdfe3ebbf8f750361988714c59e
Mateusz Wszeborowski <wszeborowskimateusz@gmail.com>
2025-02-06 15:25:01 -0800

    Fix react example app

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      react: 'preact/compat',
+      'react-dom': 'preact/compat',
+    },
+  },
+});
