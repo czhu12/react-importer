e9df3b6f83d86d9030b6f50348b4ac2d44ee4701
Chris Zhu <chris@chriss-mbp.lan>
2025-02-21 14:56:49 -0800

    example datasets

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,27 +1,12 @@
-import { useState } from 'preact/hooks';
-import Importer, { SheetState } from 'react-importer';
 import 'react-importer/dist/react-importer.css';
 import Header from './components/Header';
 import CodeBlock from './components/CodeBlock';
 import Footer from './components/Footer';
 import { EXAMPLE_CODE } from './constants';
+import EmployeeSheetImporter from './components/EmployeeSheetImporter';
+import EmployeeImporter from './components/EmployeeImporter';
 
 const App = () => {
-  const [ready, setReady] = useState(false);
-
-  const onComplete = async (
-    data: SheetState[],
-    onProgress: (progress: number) => void
-  ) => {
-    await new Promise((resolve) => setTimeout(resolve, 200));
-    onProgress(20);
-    await new Promise((resolve) => setTimeout(resolve, 200));
-    onProgress(50);
-    await new Promise((resolve) => setTimeout(resolve, 200));
-    onProgress(100);
-    console.log(data);
-    setReady(true);
-  };
 
   return (
     <div className="min-h-screen w-full">
@@ -47,98 +32,8 @@ const App = () => {
         </div>
 
         <div className="content">
-          <h1>
-            Want to see a demo? Try uploading{' '}
-            <a className="text-blue-500 hover:text-blue-600" href="/data.csv">
-              this file
-            </a>
-            .
-          </h1>
-          <Importer
-            sheets={[
-              {
-                id: 'employees',
-                label: 'Employees',
-                columns: [
-                  {
-                    label: 'Name',
-                    id: 'name',
-                    type: 'string',
-                    validators: [{ validate: 'required' }],
-                  },
-                  {
-                    label: 'Email',
-                    id: 'email',
-                    type: 'string',
-                    validators: [
-                      { validate: 'required' },
-                      { validate: 'unique', error: 'This email is not unique' },
-                      {
-                        validate: 'regex_matches',
-                        regex:
-                          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                        error: 'This email is not valid',
-                      },
-                    ],
-                  },
-                  {
-                    label: 'Phone Number',
-                    id: 'phone_number',
-                    type: 'string',
-                    validators: [{ validate: 'required' }],
-                  },
-                  { label: 'City', id: 'city', type: 'string' },
-                  {
-                    label: 'State',
-                    id: 'state',
-                    type: 'string',
-                    isReadOnly: true,
-                    transformers: [{ transformer: 'state_code' }],
-                  },
-                ],
-              },
-              {
-                id: 'companies',
-                label: 'Companies',
-                columns: [
-                  {
-                    label: 'Company',
-                    id: 'company',
-                    type: 'string',
-                    validators: [{ validate: 'required' }],
-                  },
-                  {
-                    label: 'Industry',
-                    id: 'industry',
-                    type: 'enum',
-                    typeArguments: {
-                      values: [
-                        { label: 'Tech', value: 'tech' },
-                        { label: 'Finance', value: 'finance' },
-                      ],
-                    },
-                    validators: [{ validate: 'required' }],
-                  },
-                  {
-                    label: 'Name',
-                    id: 'name',
-                    type: 'reference',
-                    typeArguments: {
-                      sheetId: 'employees',
-                      sheetColumnId: 'name',
-                    },
-                    validators: [{ validate: 'required' }],
-                  },
-                ],
-              },
-            ]}
-            onComplete={onComplete}
-          />
-          {ready && (
-            <div style={{ margin: '0 auto', maxWidth: '1200px' }}>
-              <h4>Check the console for the output!</h4>
-            </div>
-          )}
+          <EmployeeImporter />
+          <EmployeeSheetImporter />
         </div>
       </div>
 
