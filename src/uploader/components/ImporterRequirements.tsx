bda7c68e4ca783c9733caea8a7888f7c155424d1
Emily Fields <80897288+emily-fields@users.noreply.github.com>
2025-03-15 20:47:27 -0700

    styling updates

--- src/uploader/components/ImporterRequirements.tsx
+++ src/uploader/components/ImporterRequirements.tsx
@@ -14,28 +14,25 @@ const RequirementInfo = ({
 }) => (
   <div className="h-full w-full space-y-5 overflow-y-auto">
     {Object.entries(importerRequirements).map(([groupName, requirements]) => (
-      <div key={groupName}>
-        <div className="my-2 text-sm font-light uppercase">
+      <div key={groupName} className="me-3">
+        <div className="my-3 border-b border-gray-200 pb-4 text-sm font-light uppercase">
           {groupName} Columns
         </div>
-        <div className="my-2 me-3">
+        <div className="mt-4">
           {requirements.map((requirement) => (
             <div
               key={requirement.columnId}
-              className="my-2.5 flex justify-between"
+              className="my-3 flex justify-between"
             >
-              <div className="text-xs font-medium">
-                {requirement.columnLabel}
-              </div>
+              <div className="text-xs">{requirement.columnLabel}</div>
               <div className="text-xs font-light">
-                {/* TODO: Fix tooltip since it goes behind the column next to it */}
                 <Tooltip
                   tooltipText={
                     requirement?.importerInformation ||
                     `This column is ${groupName}`
                   }
                 >
-                  <InformationCircleIcon className="size-5 text-gray-400" />
+                  <InformationCircleIcon className="size-5 text-gray-500" />
                 </Tooltip>
               </div>
             </div>
