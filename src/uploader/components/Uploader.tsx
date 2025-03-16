8667620a3f8604c77df6cd612ec1866411e95723
Emily Fields <80897288+emily-fields@users.noreply.github.com>
2025-03-15 22:13:12 -0700

    fix for narrow screens

--- src/uploader/components/Uploader.tsx
+++ src/uploader/components/Uploader.tsx
@@ -24,7 +24,7 @@ export default function Uploader({
   return (
     <div className="flex h-full flex-col space-y-4">
       <div className="flex-none text-2xl">{t('uploader.uploadAFile')}</div>
-      <div className="min-h-0 flex-auto">
+      <div className="flex-auto md:min-h-0">
         <div className="flex h-full flex-col-reverse gap-5 md:flex-row">
           <div className="h-full flex-1 lg:flex-1">
             <ImporterRequirements
