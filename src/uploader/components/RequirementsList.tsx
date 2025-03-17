0188d661c503689abca9671ebe82077cefdc55b7
Emily Fields <80897288+emily-fields@users.noreply.github.com>
2025-03-17 16:28:49 -0700

    move to separate file

---
+++ src/uploader/components/RequirementsList.tsx
@@ -0,0 +1,46 @@
+import { ImporterRequirementsType } from '../types';
+import { InformationCircleIcon } from '@heroicons/react/24/outline';
+import { Tooltip } from '../../components/index';
+import { useTranslations } from '../../i18';
+
+export default function RequirementsList({
+  importerRequirements,
+}: {
+  importerRequirements: ImporterRequirementsType;
+}) {
+  const { t } = useTranslations();
+
+  return (
+    <div className="h-full w-full space-y-5 overflow-y-auto">
+      {Object.entries(importerRequirements)
+        .filter(([, requirements]) => requirements.length > 0)
+        .map(([groupName, requirements]) => (
+          <div key={groupName} className="me-3">
+            <div className="my-3 border-b border-gray-200 pb-4 text-sm font-light uppercase">
+              {t(`uploader.${groupName}Columns`)}
+            </div>
+            <div className="mt-4">
+              {requirements.map((requirement) => (
+                <div
+                  key={requirement.columnId}
+                  className="my-3 flex justify-between"
+                >
+                  <div className="text-xs">{requirement.columnLabel}</div>
+                  <div className="text-xs font-light">
+                    <Tooltip
+                      tooltipText={
+                        requirement?.importerInformation ||
+                        t(`uploader.${groupName}ColumnsTooltip`)
+                      }
+                    >
+                      <InformationCircleIcon className="size-5 text-gray-500" />
+                    </Tooltip>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+    </div>
+  );
+}
