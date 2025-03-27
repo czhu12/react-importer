87a7371fdd48a8d635a8471fb59a02e3283b7231
Mateusz Wszeborowski <wszeborowskimateusz@gmail.com>
2025-03-27 08:41:18 -0700

    Change background color on file hover (#111)
    * Change bh color on file hover
    
    * Fix file uploader padding
    
    * Fix irregular color changes
    
    * Fix type
    
    * Fix theme

--- src/uploader/components/FileUploader.tsx
+++ src/uploader/components/FileUploader.tsx
@@ -1,4 +1,4 @@
-import { useRef } from 'preact/compat';
+import { useRef, useState } from 'preact/compat';
 import { Button, Card } from '../../components';
 import { CloudArrowUpIcon } from '@heroicons/react/24/outline';
 import { useTranslations } from '../../i18';
@@ -19,8 +19,8 @@ export default function FileUploader({
   maxFileSizeInBytes,
 }: Props) {
   const { t, tHtml } = useTranslations();
-
   const fileInputRef = useRef<HTMLInputElement | null>(null);
+  const [isDragging, setIsDragging] = useState(false);
 
   // TODO: Add error handling
   const validateAndSetFile = (file: File, maxFileSizeInBytes: number) => {
@@ -41,52 +41,58 @@ export default function FileUploader({
 
   const handleDrop = (event: DragEvent) => {
     event.preventDefault();
+    setIsDragging(false);
     if (event.dataTransfer?.files.length) {
       validateAndSetFile(event.dataTransfer.files[0], maxFileSizeInBytes);
     }
   };
 
   return (
-    <Card variant="muted" className="h-full">
-      <div className="flex h-full flex-col">
-        <div
-          onClick={() => fileInputRef.current?.click()}
-          onDragOver={(e) => e.preventDefault()}
-          onDrop={(e) => handleDrop(e)}
-          className="flex h-full flex-col"
-        >
-          <div className="flex flex-1 flex-col items-center justify-center">
-            <CloudArrowUpIcon className="text-csv-importer-primary h-12 w-12" />
-            <p className="mt-3">{t('uploader.dragAndDrop')}</p>
-            <div className="mt-3 text-sm text-gray-500">
-              {tHtml('uploader.maxFileSizeInBytes', {
-                size: <b>{formatFileSize(maxFileSizeInBytes)}</b>,
-              })}{' '}
-              • CSV
-            </div>
-            <div className="mt-3">
-              <Button>{t('uploader.browseFiles')}</Button>
-            </div>
-            {allowManualDataEntry && (
-              <div className="mt-3 text-sm">
-                <p
-                  onClick={onEnterDataManually}
-                  className="text-csv-importer-primary hover:text-csv-importer-primary cursor-pointer decoration-2 opacity-90 hover:underline focus:underline focus:outline-none"
-                >
-                  {t('uploader.enterManually')}
-                </p>
-              </div>
-            )}
+    <Card variant="muted" withPadding={false} className="h-full">
+      <div
+        className={`flex h-full flex-col p-5 transition-colors ${
+          isDragging ? 'bg-csv-importer-muted-light' : 'bg-csv-importer-muted'
+        }`}
+        onClick={() => fileInputRef.current?.click()}
+        onDragOver={(e) => {
+          e.preventDefault();
+          setIsDragging(true);
+        }}
+        onDragEnter={() => setIsDragging(true)}
+        onDragLeave={() => setIsDragging(false)}
+        onDrop={(e) => handleDrop(e)}
+      >
+        <div className="flex flex-1 flex-col items-center justify-center">
+          <CloudArrowUpIcon className="text-csv-importer-primary h-12 w-12" />
+          <p className="mt-3">{t('uploader.dragAndDrop')}</p>
+          <div className="mt-3 text-sm text-gray-500">
+            {tHtml('uploader.maxFileSizeInBytes', {
+              size: <b>{formatFileSize(maxFileSizeInBytes)}</b>,
+            })}{' '}
+            • CSV
           </div>
-
-          <input
-            ref={fileInputRef}
-            type="file"
-            accept={SUPPORTED_FILE_MIME_TYPES.join(',')}
-            style={{ display: 'none' }}
-            onChange={(e) => handleFileSelect(e)}
-          />
+          <div className="mt-3">
+            <Button>{t('uploader.browseFiles')}</Button>
+          </div>
+          {allowManualDataEntry && (
+            <div className="mt-3 text-sm">
+              <p
+                onClick={onEnterDataManually}
+                className="text-csv-importer-primary hover:text-csv-importer-primary cursor-pointer decoration-2 opacity-90 hover:underline focus:underline focus:outline-none"
+              >
+                {t('uploader.enterManually')}
+              </p>
+            </div>
+          )}
         </div>
+
+        <input
+          ref={fileInputRef}
+          type="file"
+          accept={SUPPORTED_FILE_MIME_TYPES.join(',')}
+          style={{ display: 'none' }}
+          onChange={(e) => handleFileSelect(e)}
+        />
       </div>
     </Card>
   );
